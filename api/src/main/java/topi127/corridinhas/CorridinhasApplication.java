bb119c2e45446761d2e9c9a546cf74c6692533ce
Rodrigo Rodrigues <rod19122003@gmail.com>
2026-07-16 22:35:40 +0100

    app Spring API default proj

---
+++ api/src/main/java/topi127/corridinhas/CorridinhasApplication.java
@@ -0,0 +1,13 @@
+package topi127.corridinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorridinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorridinhasApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/topi127/corridinhas/CorridinhasApplicationTests.java
@@ -0,0 +1,13 @@
+package topi127.corridinhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorridinhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
